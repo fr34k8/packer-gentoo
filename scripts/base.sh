137556a6b1116875205d5b746d09c43650fb55df
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-02-13 01:19:45 +0300

    install dracut
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/base.sh
+++ scripts/base.sh
@@ -4,5 +4,7 @@ emerge --nospinner cfg-update
 emerge --nospinner --autounmask-write dracut || true
 yes 1 | cfg-update -u
 
+emerge --nospinner dracut
+
 dracut -H -f /boot/initramfs-$(uname -r).img
 grub2-mkconfig -o /boot/grub/grub.cfg
