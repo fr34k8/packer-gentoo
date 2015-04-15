72eb4e12c01fe4adfa45519bd835613ea1da50cf
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-04-15 15:29:25 +0300

    add missing dep
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/base.sh
+++ scripts/base.sh
@@ -4,7 +4,7 @@ emerge --nospinner cfg-update
 emerge --nospinner --autounmask-write dracut || true
 yes 1 | cfg-update -u
 
-emerge --nospinner dracut
+emerge --nospinner dracut curl
 
 dracut -H -f /boot/initramfs-$(uname -r).img
 grub2-mkconfig -o /boot/grub/grub.cfg
