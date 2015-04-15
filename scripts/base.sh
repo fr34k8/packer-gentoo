bd6e8f62d596ec12234b71d1ce8de97d2694682d
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-04-15 15:48:38 +0300

    fix curl
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/base.sh
+++ scripts/base.sh
@@ -4,7 +4,7 @@ emerge --nospinner cfg-update
 emerge --nospinner --autounmask-write dracut || true
 yes 1 | cfg-update -u
 
-emerge --nospinner dracut curl
+emerge --nospinner dracut net-misc/curl
 
 dracut -H -f /boot/initramfs-$(uname -r).img
 grub2-mkconfig -o /boot/grub/grub.cfg
