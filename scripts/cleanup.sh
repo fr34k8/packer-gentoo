6b382d573859b5364bd970e0eed2816fe4b09ed4
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-04-15 17:22:26 +0300

    minimize gentoo image
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -15,8 +15,17 @@ rm  -f /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
 rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
 
-
+rm -rf /usr/portage/
+cat <<EOF > /etc/local.d/portage.start
+#/bin/bash
+rm -f /etc/local.d/portage.start
+wget http://mirror.yandex.ru/gentoo-distfiles/snapshots/portage-latest.tar.bz2 -O /portage.tar.bz2
+tar -C /usr/ -jxf /portage.tar.bz2
+rm -f /portage.tar.bz2
+EOF
+chmod +x /etc/local.d/portage.start
 sync
+rc-update add local default
 
 fstrim -v / || echo dummy
 
